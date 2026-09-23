5dd45b1422ad52566dd9219d96504ae11462466d
Lautaro <lautigagar@gmail.com>
2026-09-23 16:50:38 -0300

    inicial

---
+++ dos.js
@@ -0,0 +1,30 @@
+function calcularPromedio(notas) {
+  return (
+    notas.reduce((acumulador, actual) => acumulador + actual, 0) / notas.length
+  );
+}
+
+function estaAprobado(nota, minima = 6) {
+  return nota >= minima;
+}
+
+const estudiantes = [
+  { nombre: "Lautaro", notas: [10, 9, 9, 8.5] },
+  { nombre: "Lucas", notas: [6, 5, 6, 8.5] },
+  { nombre: "Alan", notas: [3, 5, 10, 2] },
+  { nombre: "Hitoshi", notas: [7.5, 8, 9, 10] },
+  { nombre: "Ramón", notas: [10, 9, 9.5, 8] },
+];
+
+const estudiantesConPromedios = estudiantes.map((e) => ({
+  ...e,
+  promedio: calcularPromedio(e.notas),
+}));
+
+const estudiantesAprobados = estudiantesConPromedios.filter((e) =>
+  estaAprobado(e.promedio),
+);
+
+console.log(
+  `Los estudiantes aprobados son:\n${estudiantesAprobados.map((e) => `${e.nombre} con ${e.promedio} de promedio.`).join("\n")}`,
+);
---
+++ uno.js
@@ -0,0 +1,11 @@
+const nombre = "Lautaro";
+const edad = 21;
+const promedio = 10;
+
+console.log(`${nombre} con edad ${edad} tiene ${promedio} de promedio.`);
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof promedio);
+
+console.log(5 === "5");
